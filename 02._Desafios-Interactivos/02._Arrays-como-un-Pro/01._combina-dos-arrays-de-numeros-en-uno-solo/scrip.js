efd209e24e73c5fdf814d0118a0b67a3b4ee1e24
w3zeblamf <zeblamfw3@gmail.com>
2022-06-11 18:56:35 -0400

    combina-dos-arrays-de-numeros-en-uno-solo

---
+++ 02._Desafios-Interactivos/02._Arrays-como-un-Pro/01._combina-dos-arrays-de-numeros-en-uno-solo/scrip.js
@@ -0,0 +1,48 @@
+/*¡Recuerde usar el modo 'estricto' en todos los scripts ahora!
+Puede usar el modo estricto en todos sus programas. Te ayuda a escribir código más limpio,
+como evitar que uses variables no declaradas. (https://www.w3schools.com/js/js_strict.asp)*/
+'use strict';
+
+
+//**Desafío 01.- Combina dos arrays de números en uno solo
+//**=======================================================
+
+/* 
+En este desafío recibirás dos arrays de números como parámetros de entrada y debes retornar un array que tenga los dos arrays unidos.
+
+La solución debería tener un input y output como los siguientes:
+
+Input
+
+solution([1, 2, 3], [4, 5, 6]);
+solution(["A", "B", "C"], ["D", "E", "H"]);
+
+Output
+
+[1, 2, 3, 4, 5 , 6]
+["A", "B", "C", "D", "E", "H"]
+*/
+
+
+// Solución 1 (Mi Solución)
+function solution(arrayA, arrayB) {
+  const arrJoin = arrayA.concat(arrayB)
+  return arrJoin;
+}
+
+console.log(solution([1, 2, 3], [4, 5, 6]))
+console.log(solution(["A", "B", "C"], ["D", "E", "H"]))
+
+
+// Otras Soluciones
+
+// Solución 2 
+//=========================
+
+
+//Lecturas de Referencia 
+//======================
+
+//MDN Web Docs
+//=============
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
