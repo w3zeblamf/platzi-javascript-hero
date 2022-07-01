ef4d7514c22376456a51098b83fd599342d791d7
w3zeblamf <zeblamfw3@gmail.com>
2022-07-01 18:36:24 -0400

    Desafio-31.-Explica-este-codigo-JavaScript_JSON.stringify

--- 01._Un-Quiz-por-dia/un-quiz-por-dia.js
+++ 01._Un-Quiz-por-dia/un-quiz-por-dia.js
@@ -614,3 +614,23 @@ Sin usar el spread operator tendríamos un objeto anidado:
 
 
 Justamente el spread operator se encarga de expandir user para evitar el anidamiento. */
+
+
+
+//*Desafío 31.- Explica este código JavaScript_JSON.stringify_😎
+//**===========================================================
+
+
+const settings = {
+  username: "lydiahallie",
+  level: 19,
+  health: 90
+};
+
+const data = JSON.stringify(settings, ["level", "health"]);
+console.log(data); //🤔?
+
+/*
+Respuesta Correcta: A) "{"level":19, "health":90}"
+JSON.stringify puede recibir un 2do parámetro opcional denominado replacer, puede ser una función o un arreglo, y se encarga de hacer un filtro de las propiedades del objeto que deseamos convertir a string, en el ejemplo solo deseamos convertir las propiedades ["level", "health"], ignorando username.
+*/
