a49fa86ac0fb09570dc71c3019d97dfcd0ce4de5
w3zeblamf <zeblamfw3@gmail.com>
2022-07-03 18:40:12 -0400

    Desafio-33.-Explica-este-codigo-JavaScript_funciones

--- 01._Un-Quiz-por-dia/un-quiz-por-dia.js
+++ 01._Un-Quiz-por-dia/un-quiz-por-dia.js
@@ -656,3 +656,30 @@ La primera función en llamarse es increaseNumber que solo se encarga de retorna
 
 num1 se pasa como parámetro a increasePassedNumber que hace lo mismo que increaseNumber, regresa primero el valor de la variable y luego la incrementa, por ello obtenemos nuevamente como salida el valor 10.
 */
+
+//*Desafío 33.- Explica este código JavaScript_funciones_😎
+//**========================================================
+
+const value = { number: 10 };
+
+const multiply = (x = { ...value }) => {
+  console.log((x.number *= 2));
+};
+
+multiply(); //🤔?
+multiply(); //🤔?
+multiply(value); //🤔?
+multiply(value); //🤔?
+
+/*
+Respuesta Correcta: C) 20, 20, 20, 40
+Hay que concentrarse en el orden en que se llaman las funciones para comprender que es lo que pasa acá.
+
+Primera llamada: A multiply no le pasamos ningún parámetro, por ende, toma el parámetro por defecto x que es un objeto desestructurado cuya key number tiene el valor de 10. Entonces x.number *= 2 nos retorna 20.
+
+Segunda llamada: Similar a la primera llamada, hacemos lo mismo, entonces obtenemos nuevamente 20.
+
+Tercera llamada: A multiply en su llamada le pasamos el argumento value por lo que la función ahora ignora el parámetro por defecto. number es nuevamente 10, por ello el resultado de la multiplicación nuevamente será 20.
+
+Cuarta llamada: Similar a la tercera llamada, pero el valor de value actual es 20 que fue el resultado de la tercera llamada, entonces ahora x.number *= 2, será 40.
+*/
