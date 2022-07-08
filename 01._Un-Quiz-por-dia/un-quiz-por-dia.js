410201908fcb075482cf1671b3d4814813d7077f
w3zeblamf <zeblamfw3@gmail.com>
2022-07-07 23:52:46 -0400

    Desafio-36.-Que-devuelve-el-siguiente-codigo_arrow-functions

--- 01._Un-Quiz-por-dia/un-quiz-por-dia.js
+++ 01._Un-Quiz-por-dia/un-quiz-por-dia.js
@@ -741,3 +741,25 @@ const two = makeMulti(2); */
 
 
 //https://developer.mozilla.org/es/docs/Web/JavaScript/Closures
+
+
+//*Desafío 36.- ¿Cuál es el resultado del siguiente código?_arrow functions_😎
+//**===========================================================================
+
+let Alimento = function (comida) {
+  this.comida = comida;
+};
+
+Alimento.prototype.comerHoy = () => console.log('Hoy toca comer:', this.comida);
+const alimento = new Alimento('ensalada');
+
+alimento.comerHoy(); //❓
+
+/*
+
+Respuesta Correcta: D) Hoy toca comer: undefined
+Una de las limitaciones de las funciones flecha (arrow functions), es que no tiene sus propias referencias a this o super y no se debe usar como métodos.
+
+this siempre apuntará a this desde el contexto en el que definimos la función (ejemplo window).
+
+Por tal razón, la función flecha () => { ... } no sabe de this.comida */
