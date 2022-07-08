88e08a9a46bc8aae409999ed260cffba127a18f0
w3zeblamf <zeblamfw3@gmail.com>
2022-07-08 13:30:34 -0400

    Desafio-37.-Explica-este-codigo-JavaScript_for_in-of

--- 01._Un-Quiz-por-dia/un-quiz-por-dia.js
+++ 01._Un-Quiz-por-dia/un-quiz-por-dia.js
@@ -763,3 +763,25 @@ Una de las limitaciones de las funciones flecha (arrow functions), es que no tie
 this siempre apuntará a this desde el contexto en el que definimos la función (ejemplo window).
 
 Por tal razón, la función flecha () => { ... } no sabe de this.comida */
+
+//*Desafío 37.- Explica este código JavaScript_for_in-of_😎
+//**========================================================
+
+const myLifeSummedUp = ["☕", "💻", "🍷", "🍫"];
+
+for (let item in myLifeSummedUp) {
+  console.log(item); //🤔?
+}
+
+for (let item of myLifeSummedUp) {
+  console.log(item); //🤔?
+}
+
+/* 
+Respuesta Correcta: A) 0 1 2 3 y "☕" "💻" "🍷" "🍫"
+Con el bucle for-in, podemos iterar sobre propiedades enumerables. Los enumerables en el arreglo son justamente sus índices. Por ello es 0 1 2 3.
+
+Con un bucle for-of, podemos recorrer iterables. Un arreglo por definición es un iterable, en cada iteración la variable item es igual al elemento sobre el cual se itera en ese momento. Por ello es "☕" "💻" "🍷" "🍫".
+
+En la practica los bucles for-of son más usados y ocasionalmente los bucles for-in. 
+*/
