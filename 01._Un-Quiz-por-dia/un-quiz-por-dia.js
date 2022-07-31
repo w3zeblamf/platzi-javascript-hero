8384f9f6075c979ca8cb480a890cd2311e64326d
w3zeblamf <zeblamfw3@gmail.com>
2022-07-31 16:04:01 -0400

    Desafio-45.-Explica-este-codigo-JavaScript_TypeError

--- 01._Un-Quiz-por-dia/un-quiz-por-dia.js
+++ 01._Un-Quiz-por-dia/un-quiz-por-dia.js
@@ -902,13 +902,16 @@ const colorConfig = {
 };
 
 const colors = ['pink', 'red', 'blue'];
+
 console.log(colorConfig.colors[1]);
 
+console.log(colorConfig[colors[1]]);
+
 /* Respuesta Correcta: D) TypeError
+
 En javascript existen 2 maneras de acceder a las propiedades de los objetos, por notación del punto o por notación de corchetes.
 
 Cuando hacemos colorConfig.colors[1] literalmente estamos buscando una propiedad colors en el objeto colorConfig y como no existe esta propiedad entonces obtenemos un undefiend, entonces ahora javascript intentará hacer undefined[1] y esto no es un código valido, por ello la consola muestra un TypeError.
 
 Cuando queremos usar variables para hacer lo que se denomina acceso a propiedades dinámicas de objetos necesitamos usar la notación de corchetes: colorConfig[colors[1]] que nos devolverá true, el valor de la propiedad red del objeto colorConfig.
-
 */
